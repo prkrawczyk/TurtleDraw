9f7a5d420a34bf92e8eeee516f9122c26cc9a278
Patryk Krawczyk <prkrawczyk724@gmail.com>
2020-11-17 21:17:52 -0600

    Add files via upload

---
+++ turtledraw.py
@@ -0,0 +1,103 @@
+import turtle
+TedTurtle = turtle.Turtle()
+TedTurtle.pensize(3)
+
+TedTurtle.penup()
+TedTurtle.pencolor("green")
+TedTurtle.goto(36,29)
+TedTurtle.pendown()
+TedTurtle.goto(-49, 128)
+TedTurtle.goto(-16, 36)
+
+TedTurtle.penup()
+
+TedTurtle.goto(54, 71)
+TedTurtle.pendown()
+TedTurtle.pencolor("black")
+TedTurtle.goto(-126, 15)
+TedTurtle.goto(180, 177)
+TedTurtle.goto(-57, -82)
+
+TedTurtle.penup()
+
+TedTurtle.goto(-89, -50)
+TedTurtle.pendown()
+TedTurtle.pencolor("green")
+TedTurtle.goto(-155, 61)
+TedTurtle.pencolor("red")
+TedTurtle.goto(-78, -187)
+TedTurtle.pencolor('blue')
+TedTurtle.goto(72, 161)
+
+TedTurtle.penup()
+
+TedTurtle.goto(-160, -29)
+TedTurtle.pendown()
+TedTurtle.pencolor("red")
+TedTurtle.goto(-103, 192)
+TedTurtle.goto(-182, 162)
+TedTurtle.goto(-15, 145)
+
+TedTurtle.penup()
+
+TedTurtle.goto(13, -97)
+TedTurtle.pendown()
+
+TedTurtle.penup()
+
+TedTurtle.goto(168, -197)
+TedTurtle.pendown()
+TedTurtle.pencolor("green")
+TedTurtle.goto(48, 1)
+TedTurtle.pencolor("blue")
+TedTurtle.goto(50, 148)
+TedTurtle.pencolor("green")
+TedTurtle.goto(102, -175)
+TedTurtle.pencolor("black")
+TedTurtle.goto(133, -115)
+TedTurtle.goto(52, -99)
+
+TedTurtle.penup()
+
+TedTurtle.goto(130, 136)
+TedTurtle.pendown()
+TedTurtle.pencolor("blue")
+TedTurtle.goto(39, -191)
+TedTurtle.pencolor("green")
+TedTurtle.goto(-58, -159)
+
+TedTurtle.penup()
+
+TedTurtle.goto(53, 139)
+TedTurtle.pendown()
+TedTurtle.pencolor("green")
+TedTurtle.goto(6, 175)
+TedTurtle.goto(-138, 82)
+
+TedTurtle.penup()
+
+TedTurtle.goto(87, 33)
+TedTurtle.pendown()
+TedTurtle.pencolor("blue")
+TedTurtle.penup()
+
+TedTurtle.goto(144, 71)
+TedTurtle.pendown()
+TedTurtle.pencolor("black")
+TedTurtle.goto(-57, 72)
+
+TedTurtle.penup()
+
+TedTurtle.goto(-22, 61)
+TedTurtle.pendown()
+TedTurtle.pencolor("blue")
+TedTurtle.goto(58, 164)
+
+TedTurtle.penup()
+
+TedTurtle.goto(-45, 26)
+TedTurtle.pendown()
+TedTurtle.pencolor("blue")
+TedTurtle.goto(107, 110)
+
+turtle.done()
